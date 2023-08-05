2f1dc6244c4086a080ecadb3c3a2def08d89c96f
João Costa <joaocosta@dcc.ufmg.br>
2023-08-05 14:15:44 -0300

    Adicionando settings.hpp

---
+++ CPP/maze/include/settings.hpp
@@ -0,0 +1,12 @@
+const float CELL_SIZE = 40.0f;
+const Rectangle Menu = Rectangle {}; 
+const Color goal_color = Color{255, 0, 0, 255};
+const Color start_color = Color{0, 171, 28, 255};
+const Color wall_color = Color{40, 40, 40, 255};
+const Color free_color = Color{237, 240, 252, 255};
+const Color solution_color = Color{220, 235, 113, 255};
+const Color explored_color = Color{212, 97, 85, 255};
+const Color frontier_color = Color{255, 150, 130, 255};
+
+const Color stats_color = Color{237, 240, 252, 255};
+const float stats_height = 30.0f;
--- CPP/maze/src/main.cpp
+++ CPP/maze/src/main.cpp
@@ -1,58 +1,6 @@
 #include "maze.hpp"
-#include "maze-app.hpp"
-#include "CLI11.hpp" 
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-    CLI::App app("Maze Solver");
-
-    std::string option;
-    std::string input_maze;
-    std::string frontier_option;  // Option to specify stack or queue
-
-    app.add_option("--option", option, "Choose option: maze or mazeApp")->required();
-    app.add_option("--input_maze", input_maze, "Input maze file")->required();
-    app.add_option("--frontier", frontier_option, "Frontier type: stack or queue")->required();
-
-    CLI11_PARSE(app, argc, argv);
-
-    char frontier_key;
-    if (!frontier_option.empty()) {
-        if (frontier_option == "stack") {
-            frontier_key = 's';
-        } else if (frontier_option == "queue") {
-            frontier_key = 'q';
-        } else {
-            std::cerr << "Invalid frontier option: " << frontier_option << std::endl;
-            exit(1);
-        }
-    }
-
-    if (option == "maze") {
-        // execute Maze
-        // read
-        Maze maze(input_maze, frontier_key);
-
-        // solve
-        maze.solve();
-
-        // write
-        size_t pos = input_maze.find_last_of(".");
-        std::string output_maze = input_maze.substr(0, pos) + "-solution.txt";
-        maze.write_solution(output_maze);
-
-        std::cout << "Maze solved successfully!" << std::endl;
-    } else if (option == "mazeApp") {
-        // execute MazeApp
-        MazeApp mazeApp(input_maze, frontier_key);
-
-        mazeApp.run();
-
-        std::cout << "MazeApp executed successfully!" << std::endl;
-    } else {
-        std::cerr << "Options: mazeApp, maze" << option << std::endl;
-        exit(1);
-    }
-
+int main() {
     return 0;
 }
